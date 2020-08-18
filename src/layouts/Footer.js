37ece1acc2ae217152850cc0203c9b18b20364b8
Kamil Skorzynski <skorzynskikamil@gmail.com>
2020-08-18 21:18:16 +0200

    icons hover

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -10,13 +10,13 @@ const Footer = () => {
             href="https://www.instagram.com/misiek.zdzisiek/"
             className="footer__link"
           >
-            <i class="fab fa-instagram"></i>
+            <i class="fab fa-instagram icons__link"></i>
           </a>
           <a href="https://www.facebook.com/majkbush" className="footer__link">
-            <i class="fab fa-facebook"></i>
+            <i class="fab fa-facebook icons__link"></i>
           </a>
           <a href="https://github.com/skorzyk" className="footer__link">
-            <i class="fab fa-github"></i>
+            <i class="fab fa-github icons__link"></i>
           </a>
         </div>
         <p className="footer__desc">Copyrigth @ Kamil Skórzyński 2020</p>
