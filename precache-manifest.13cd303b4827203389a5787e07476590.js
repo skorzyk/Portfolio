7257962529d617b8d38c1d950bde54d8adec9e92
Kamil Skorzynski <skorzynskikamil@gmail.com>
2020-08-18 15:48:40 +0200

    Updates

---
+++ precache-manifest.13cd303b4827203389a5787e07476590.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35a096b26b138d8025c253437cbbaaa",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ce3d03204dca7773e58b",
+    "url": "/Portfolio/static/css/2.8484b046.chunk.css"
+  },
+  {
+    "revision": "752acb17b3c1a7574fb2",
+    "url": "/Portfolio/static/css/main.a5383294.chunk.css"
+  },
+  {
+    "revision": "ce3d03204dca7773e58b",
+    "url": "/Portfolio/static/js/2.925752bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.925752bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752acb17b3c1a7574fb2",
+    "url": "/Portfolio/static/js/main.5487426b.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "5af223af959297accd29dcb9d13c8c41",
+    "url": "/Portfolio/static/media/2667878.5af223af.svg"
+  },
+  {
+    "revision": "3782da7b9cc617c818d83d1e657b0fbe",
+    "url": "/Portfolio/static/media/CV_Kamil_Skorzynski.3782da7b.pdf"
+  },
+  {
+    "revision": "6e798ff4c753fe69537b3b0b1fb3492a",
+    "url": "/Portfolio/static/media/alarm.6e798ff4.png"
+  },
+  {
+    "revision": "40a7d60ae24a5bd0dfc3345c66cc97d5",
+    "url": "/Portfolio/static/media/budget.40a7d60a.png"
+  },
+  {
+    "revision": "e084de92b0b9309f61b9166fe923e189",
+    "url": "/Portfolio/static/media/canvas.e084de92.png"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/Portfolio/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/Portfolio/static/media/html.31039a53.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/Portfolio/static/media/javascript.6a4c201c.png"
+  },
+  {
+    "revision": "335496ba53f3e174260e936e87514de9",
+    "url": "/Portfolio/static/media/misiek1.335496ba.png"
+  },
+  {
+    "revision": "3b0d0450b42f0d25530ad589d502482f",
+    "url": "/Portfolio/static/media/mockup.3b0d0450.jpg"
+  },
+  {
+    "revision": "efd22267efa1c9589fe5611454dfe821",
+    "url": "/Portfolio/static/media/mysql.efd22267.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/Portfolio/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "9678898deed4bf839a6a8db4a5f02bb3",
+    "url": "/Portfolio/static/media/pokemony.9678898d.png"
+  },
+  {
+    "revision": "833bbfcb2c1a840edfa2b04b19c7ce14",
+    "url": "/Portfolio/static/media/programming.833bbfcb.jpg"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/Portfolio/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "a2bc8ecf6d9052818b36402f0431c60f",
+    "url": "/Portfolio/static/media/sass.a2bc8ecf.png"
+  },
+  {
+    "revision": "78c7d795707a4df6e7eddf3feeec7c88",
+    "url": "/Portfolio/static/media/szkicownik.78c7d795.png"
+  }
+]);
